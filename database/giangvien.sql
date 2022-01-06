be9daf07dbb0902b020973ed53d7df660099689c
Dang-22 <dangkhachung10k@gmail.com>
2022-01-06 13:23:14 +0700

    push database

---
+++ database/giangvien.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th1 06, 2022 lúc 07:14 AM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `1951060733_university`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `giangvien`
+--
+
+CREATE TABLE `giangvien` (
+  `magv` int(11) NOT NULL,
+  `hovaten` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ngaysinh` date DEFAULT NULL,
+  `gioitinh` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `trinhdo` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `chuyenmon` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `hocham` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `hocvi` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `coquan` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `giangvien`
+--
+
+INSERT INTO `giangvien` (`magv`, `hovaten`, `ngaysinh`, `gioitinh`, `trinhdo`, `chuyenmon`, `hocham`, `hocvi`, `coquan`) VALUES
+(1, 'Đặng Khắc Hùng', '2012-01-04', 'Nam', 'Đại Học', 'Công nghệ thông', 'Không', 'Không', 'Khoa CNTT'),
+(2, 'Nguyễn Thị Hòa', '2012-01-05', 'Nữ', 'Đại Học', 'Kinh Tế', 'Không', 'Không', 'Khoa KT');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `giangvien`
+--
+ALTER TABLE `giangvien`
+  ADD PRIMARY KEY (`magv`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `giangvien`
+--
+ALTER TABLE `giangvien`
+  MODIFY `magv` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
